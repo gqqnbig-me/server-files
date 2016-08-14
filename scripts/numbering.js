d59b81bf7154a15304cbf6b29a76ec56541c382c
gqqnbig <gqqnb2005@gmail.com>
2016-08-13 23:06:37 -0700

    添加自动编号脚本

---
+++ scripts/numbering.js
@@ -0,0 +1,28 @@
+﻿var counters = {};
+var counterStyles = {};
+
+function label(counter, labelName, locale, options)
+{
+    if (typeof (counters[counter]) === "undefined")
+        counters[counter] = 1;
+    else
+        counters[counter]++;
+
+    if(locale!==undefined )
+        counterStyles[counter] = { locale: locale };
+    if (options !== undefined)
+        counterStyles[counter] = { options:options };
+
+    document.write("<span id='numbering-" + labelName + "'>" + new Number(counters[counter]).toLocaleString(counterStyles[counter].locale, counterStyles[counter].options) + "</span>");
+    document.currentScript.remove();
+}
+
+function ref(labelName)
+{
+    var currentElement = document.currentScript;
+    document.addEventListener("DOMContentLoaded", function()
+    {
+        currentElement.insertAdjacentHTML('afterend', "<a href='#numbering-" + labelName + "'>" + document.getElementById("numbering-" + labelName).innerText + "</a>");
+        currentElement.remove();
+    });
+}
